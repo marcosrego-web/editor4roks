332e55b2126fd4d4427549d30d885519626c9c3f
Marcos Rego <43761910+marcosrego-web@users.noreply.github.com>
2019-05-02 22:49:44 +0100

    Update editor4roks.php
    Added a different image selector when using Joomla's default editor, to avoid the conflict between the editor and the media manager inside Roksprocket.

--- editor4roks-joomla/editor4roks.php
+++ editor4roks-joomla/editor4roks.php
@@ -17,35 +17,41 @@ if($app->isAdmin()) {
 			$url = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 			$editor4roksContent  = '';
 			if (strpos($url,'com_roksprocket') !== false) {
-				$e4rVersion = 'v211';
+				$e4rVersion = 'v220';
 				$rootPath = JPATH_ROOT;
 				$rootUrl = JUri::root();
 				$pluginUrl = $rootUrl.'plugins/system/editor4roks/';
 				$editor4roksContent  = '';
 				$editoroption = $this->params->get('editoroption', 1);
+				$roks4mob = $this->params->get('roks4mob', 1);
 				if ( $editoroption != 0 || $editoroption == null) {
 					//Default Editor
 					$editor = JFactory::getEditor();
 					$editor = $editor->display('e4r_textarea', '', '550', '400', '60', '20', true);
 				} else {
 					//NicEdit Editor
-					$filesFolder = "/images";
-					$folder = $rootPath.$filesFolder;
-					$getFiles = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder), RecursiveIteratorIterator::SELF_FIRST);
-					$editor4roksContent = $editor4roksContent.'<datalist id="imagelist">';
+					JHtml::script($pluginUrl.'assets/js/nicEdit.js');
+					$editor = '<textarea id="e4r_textarea"></textarea>';
+				}
+				$filesFolder = "/images";
+				$folder = $rootPath.$filesFolder;
+				$getFiles = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder), RecursiveIteratorIterator::SELF_FIRST);
+				$editor4roksContent = $editor4roksContent.'<datalist id="imagelist">';
 					foreach($getFiles as $image){
 						if(substr($image, -4) == '.jpg' || substr($image, -5) == '.jpeg' || substr($image, -4) == '.png' || substr($image, -4) == '.gif' || substr($image, -4) == '.JPG' || substr($image, -5) == '.JPEG' || substr($image, -4) == '.PNG' || substr($image, -4) == '.GIF') {
 							$image = str_replace($rootPath."/","",$image);
-							$image = $rootUrl.$image;
+							if ( $editoroption != 0 || $editoroption == null) { } else {
+								$image = $rootUrl.$image;
+							}
 							$imagename = basename($image);
 							$editor4roksContent = $editor4roksContent.'<option value="'.$image.'">🖼 Image - '.$imagename.'</option>';
 						}
 					}
-					$editor4roksContent = $editor4roksContent.'</datalist>';
-					JHtml::script($pluginUrl.'assets/js/nicEdit.js');
-					$editor = '<textarea id="e4r_textarea"></textarea>';
-				}
+				$editor4roksContent = $editor4roksContent.'</datalist>';
 				JHtml::stylesheet($pluginUrl.'assets/css/'.$e4rVersion.'.css');
+				if ( $roks4mob != 0 || $roks4mob == null) {
+					JHtml::stylesheet($pluginUrl.'assets/css/roks4mob_'.$e4rVersion.'.css');
+				}
 				JHtml::_('jquery.framework');
 				JHtml::script($pluginUrl.'assets/js/'.$e4rVersion.'.js');
 				//Get menu list for the link editor and for NicEdit
@@ -65,6 +71,9 @@ if($app->isAdmin()) {
 					}
 				$editor4roksContent = $editor4roksContent. '</datalist>';
 				$editor4roksContent = $editor4roksContent.'<div class="linkeditor4roks"><div class="e4r-container"><div class="e4r-content"><input id="e4r-linkarea" placeholder="Search here for a menu or insert the link..." list="menufilelist"></input><div class="e4r-btn-container"><a class="btn btn-primary btn-insert"><small>✔ INSERT</small></a> <a class="btn btn-cancel"><small>✖ CANCEL</small></a></div></div></div></div>';
+				if ( $editoroption != 0 || $editoroption == null) {
+					$editor4roksContent = $editor4roksContent.'<div class="imgeditor4roks"><div class="e4r-container"><div class="e4r-content"><input id="e4r-imgarea" placeholder="Search here for an image or insert the link..." list="imagelist"></input><div class="e4r-btn-container"><div class="imgeditor4roks-preview"></div><a class="btn btn-primary btn-insert"><small>✔ INSERT</small></a> <a class="btn btn-cancel"><small>✖ CANCEL</small></a></div></div></div></div>';
+				}
 				$editor4roksContent = $editor4roksContent.'<div class="editor4roks"><div class="e4r-container"><div class="e4r-content"><div class="e4r-txt-container">'.$editor.'<div class="e4r-btn-container"><a class="btn btn-primary btn-insert"><small>✔ INSERT</small></a> <a class="btn btn-cancel"><small>✖ CANCEL</small></a></div></div></div></div>';
 			}
 		}
